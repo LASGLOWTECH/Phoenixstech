7423c7df2de272baa5a792e2a6b5d391609d5b2a
LASGLOWTECH <omozemojeau@gmail.com>
2025-05-13 09:26:21 +0100

    aded remote address

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -12,7 +12,7 @@ const isLocalhost = Boolean(
 );
 const API_URL = isLocalhost
     ? "http://localhost:5000"
-    : "https://supablogapi.supatechie.ga/api/";
+    : "https://nodeserver.phoenixstech.com";
 // const API_URL = "http://localhost:5000";
 
 const instance = axios.create({
